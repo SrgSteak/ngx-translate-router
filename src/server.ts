0bc463f56ff4e351de2017bb50f9528b2901a8ab
Robin Schmidt <schmidt91robin@icloud.com>
2026-06-25 13:55:38 +0200

    added more help

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ export function app(): express.Express {
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  const commonEngine = new CommonEngine({ allowedHosts: ['localhost']});
+  const commonEngine = new CommonEngine({ allowedHosts: ['*', '127.0.0.1', 'localhost']});
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
